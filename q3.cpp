3265178b3f7cbf5f96216cb4a5ed5d5a86df3d6a
Peal Hassan <pealhasan6@gmail.com>
2023-06-06 14:28:25 +0600

    Add files via upload

--- q3.cpp
+++ q3.cpp
@@ -2,14 +2,13 @@
 #define ll long long int 
 using namespace std;  
 void CRC(string dataword, string divisor) {
-    string temp = dataword;
-    for(ll i = 0; i < dataword.size()-1; i++)  dataword += "0";
+    for(ll i = 0; i < divisor.size()-1; i++)  dataword += "0";
     ll start = 0;
-    ll n = dataword.size();     
+    ll n = divisor.size();     
     while(start + n <= dataword.size()) {
         ll flag = 0;   
         for(ll i = start, j = 0; j < n; j++,i++) {
-            if(dataword[i] == temp[j]) dataword[i] = '0';  
+            if(dataword[i] == divisor[j]) dataword[i] = '0';  
             else dataword[i] = '1';
             if(dataword[i] == '1' && flag == 0) {
                 flag = 1;  
@@ -18,7 +17,10 @@ void CRC(string dataword, string divisor) {
         }
     }
     string res = "";  
+    string pre = "";   
     for(ll i = start; i < dataword.size(); i++) res += dataword[i];  
+    for(ll i = 0; i<(n-1-res.size()); i++) pre += "0";
+    res = pre + res;  
     cout << res << endl;  
 }
 int main() {
