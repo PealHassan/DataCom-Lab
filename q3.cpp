3be1189b3c522389b54a93df28daa093481ade97
Peal Hassan <pealhasan6@gmail.com>
2023-06-06 15:03:25 +0600

    Done

--- q3.cpp
+++ q3.cpp
@@ -1,31 +1,37 @@
 #include<bits/stdc++.h>
 #define ll long long int 
 using namespace std;  
-void CRC(string dataword, string divisor) {
-    for(ll i = 0; i < divisor.size()-1; i++)  dataword += "0";
+string CRC(string dataword, string divisor, ll mode) {
+    string temp = dataword;
+    if(mode) for(ll i = 0; i < divisor.size()-1; i++)  dataword += "0";
     ll start = 0;
     ll n = divisor.size();     
     while(start + n <= dataword.size()) {
-        ll flag = 0;   
+        ll flag = start+n;   
         for(ll i = start, j = 0; j < n; j++,i++) {
             if(dataword[i] == divisor[j]) dataword[i] = '0';  
             else dataword[i] = '1';
-            if(dataword[i] == '1' && flag == 0) {
-                flag = 1;  
-                start = i;
+            if(dataword[i] == '1' && flag == start+n) {
+                flag = i; 
             }
         }
+        start = flag;   
     }
     string res = "";  
-    string pre = "";   
+    string pre = "";  
     for(ll i = start; i < dataword.size(); i++) res += dataword[i];  
-    for(ll i = 0; i<(n-1-res.size()); i++) pre += "0";
-    res = pre + res;  
-    cout << res << endl;  
+        for(ll i = 0; i<(n-1-res.size()); i++) pre += "0";
+        res = pre + res;  
+        return temp + res; 
+    
+    
+
 }
 int main() {
     string dataword, divisor;
     cin >> dataword >> divisor;   
-    CRC(dataword,divisor);
+    string codeword = CRC(dataword,divisor,1);
+    cout << codeword << endl;  
+    cout << CRC(codeword,divisor,0) << endl;
 
 }
